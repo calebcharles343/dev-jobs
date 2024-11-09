afc5f03f9935f62ee387aefc253e470e05322039
Charles Caleb <charlesofficial9@gmail.com>
2024-11-09 11:39:29 +0100

    update

--- src/styles/MediaQuery.ts
+++ src/styles/MediaQuery.ts
@@ -1,6 +1,6 @@
 const media = {
-  tablet: "@media (max-width: 769px)",
-  mobile: "@media (max-width: 600px)",
+  tablet: "@media (max-width: 800px)",
+  mobile: "@media (max-width: 769px)",
 };
 
 export default media;
